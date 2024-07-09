97fc735c01b9bda8142083d79949930abc7ca170
hayatwhite <killerpearnation@gmail.com>
2024-07-08 21:31:37 -0700

    UI Changes/Updates
    Mainly messing around attempting various UI changes

--- capstone/Project work solo/ui.c
+++ capstone/Project work solo/ui.c
@@ -12,7 +12,6 @@ void AddLog(HWND hwnd, const char *text);
 void Connect(HWND hwnd);
 void ListFiles(HWND hwnd);
 
-
 #pragma comment(lib, "user32.lib")
 #pragma comment(lib, "kernel32.lib")
 // Global variables
@@ -52,19 +51,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         return 0;
     }
 
-   // Create textboxes for IP and Port
-    g_hIPTextBox = CreateWindow("EDIT", NULL, WS_VISIBLE | WS_CHILD | WS_BORDER,
-                                20, 20, 200, 25, hwnd, NULL, NULL, NULL);
-    g_hPortTextBox = CreateWindow("EDIT", NULL, WS_VISIBLE | WS_CHILD | WS_BORDER,
-                                  240, 20, 100, 25, hwnd, NULL, NULL, NULL);
-
-    // Create Connect button
-    g_hConnectButton = CreateWindow("BUTTON", "Connect", WS_VISIBLE | WS_CHILD,
-                                    360, 20, 100, 25, hwnd, NULL, NULL, NULL);
-
-    // Create listbox to display file system
-    g_hListBox = CreateWindow("LISTBOX", NULL, WS_VISIBLE | WS_BORDER | WS_CHILD | WS_VSCROLL | ES_AUTOVSCROLL,
-                              20, 70, 740, 400, hwnd, NULL, NULL, NULL);
+    InitUI(hwnd);
 
     // Show the window
     ShowWindow(hwnd, nCmdShow);
@@ -89,6 +76,21 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
                 ListFiles(hwnd);
             }
             return 0;
+        case WM_SIZE: {
+            RECT rect;
+            GetClientRect(hwnd, &rect);
+
+            int width = rect.right - rect.left;
+            int height = rect.bottom - rect.top;
+
+            // Resize the controls based on the new window size
+            MoveWindow(g_hIPTextBox, 20, 20, 200, 25, TRUE);
+            MoveWindow(g_hPortTextBox, 240, 20, 100, 25, TRUE);
+            MoveWindow(g_hConnectButton, 360, 20, 100, 25, TRUE);
+            MoveWindow(g_hListBox, 20, 70, width - 40, height - 90, TRUE);
+
+            return 0;
+        }
         case WM_DESTROY:
             PostQuitMessage(0);
             return 0;
@@ -97,33 +99,31 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
 }
 
 void InitUI(HWND hwnd) {
-    // Create a listbox to display logs
-    g_hLog = CreateWindow("LISTBOX", NULL, WS_VISIBLE | WS_BORDER | WS_CHILD | WS_VSCROLL | ES_AUTOVSCROLL,
-        20, 20, 740, 400, hwnd, NULL, NULL, NULL);
+    // Create textboxes for IP and Port
+    g_hIPTextBox = CreateWindow("EDIT", NULL, WS_VISIBLE | WS_CHILD | WS_BORDER,
+                                20, 20, 200, 25, hwnd, NULL, NULL, NULL);
+    g_hPortTextBox = CreateWindow("EDIT", NULL, WS_VISIBLE | WS_CHILD | WS_BORDER,
+                                  240, 20, 100, 25, hwnd, NULL, NULL, NULL);
 
-    // Create host and port edit boxes
-    g_hHostEdit = CreateWindow("EDIT", NULL, WS_VISIBLE | WS_CHILD | WS_BORDER,
-        20, 440, 200, 25, hwnd, NULL, NULL, NULL);
-    g_hPortEdit = CreateWindow("EDIT", NULL, WS_VISIBLE | WS_CHILD | WS_BORDER,
-        240, 440, 100, 25, hwnd, NULL, NULL, NULL);
+    // Create Connect button
+    g_hConnectButton = CreateWindow("BUTTON", "Connect", WS_VISIBLE | WS_CHILD,
+                                    360, 20, 100, 25, hwnd, (HMENU)1, NULL, NULL);
 
-    // Create connect and list files buttons
-    g_hConnectBtn = CreateWindow("BUTTON", "Connect", WS_VISIBLE | WS_CHILD,
-        360, 440, 100, 25, hwnd, (HMENU)1, NULL, NULL);
-    g_hUpListBtn = CreateWindow("BUTTON", "List Files", WS_VISIBLE | WS_CHILD,
-        480, 440, 100, 25, hwnd, (HMENU)2, NULL, NULL);
+    // Create listbox to display file system
+    g_hListBox = CreateWindow("LISTBOX", NULL, WS_VISIBLE | WS_BORDER | WS_CHILD | WS_VSCROLL | ES_AUTOVSCROLL,
+                              20, 70, 740, 400, hwnd, NULL, NULL, NULL);
 }
 
 void AddLog(HWND hwnd, const char *text) {
-    SendMessage(g_hLog, LB_ADDSTRING, 0, (LPARAM)text);
+    SendMessage(g_hListBox, LB_ADDSTRING, 0, (LPARAM)text);
 }
 
 void Connect(HWND hwnd) {
     // Get host and port from edit boxes
     char host[256];
     char port[10];
-    GetWindowText(g_hHostEdit, host, sizeof(host));
-    GetWindowText(g_hPortEdit, port, sizeof(port));
+    GetWindowText(g_hIPTextBox, host, sizeof(host));
+    GetWindowText(g_hPortTextBox, port, sizeof(port));
 
     // Connect code here...
 
